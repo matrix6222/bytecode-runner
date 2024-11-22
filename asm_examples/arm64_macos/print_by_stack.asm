27640a54c4925ef0ce90f27839b5d8ca685cf1e8
root <mkucharczyk2001@gmail.com>
2024-11-22 03:45:06 +0100

    added python bytecode

--- asm_examples/arm64_macos/print_by_stack.asm
+++ asm_examples/arm64_macos/print_by_stack.asm
@@ -12,4 +12,10 @@ print:
 	RET
 
 
-;;;
+; code = b''
+; code += b'\xE2\x03\x00\xAA'  # MOV X2, X0
+; code += b'\x20\x00\x80\xD2'  # MOV X0, #1
+; code += b'\xE1\x03\x00\x91'  # MOV X1, SP
+; code += b'\x90\x00\x80\xD2'  # MOV X16, #4
+; code += b'\xE1\xFF\x1F\xD4'  # SVC #0xFFFF
+; code += b'\xC0\x03\x5F\xD6'  # RET
