5ae6932880a2b2d8b6981ded01f5ad29bb9b5873
root <mkucharczyk2001@gmail.com>
2024-11-22 03:42:11 +0100

    added python bytecode

--- asm_examples/arm64_macos/print_adr_autolen.asm
+++ asm_examples/arm64_macos/print_adr_autolen.asm
@@ -4,7 +4,7 @@
 print:
 	MOV X0, #1
 	ADR X1, msg
-	MOV X2, #13
+	MOV X2, #msg_len
 	MOV X16, #4
 	SVC #0xFFFF
 
@@ -12,3 +12,16 @@ print:
 
 msg: .ascii "Hello world!\n"
 .equ msg_len, . -msg
+
+
+; code = b''
+; code += b'\x20\x00\x80\xD2'  # MOV X0, #1
+; code += b'\xA1\x00\x00\x10'  # ADR X1, #20
+; code += b'\xA2\x01\x80\xD2'  # MOV X2, #13  ; compiler automatically inserted #13 here
+; code += b'\x90\x00\x80\xD2'  # MOV X16, #4
+; code += b'\xE1\xFF\x1F\xD4'  # SVC #0xFFFF
+; code += b'\xC0\x03\x5F\xD6'  # RET
+; code += b'\x48\x65\x6C\x6C'  # H e l l
+; code += b'\x6F\x20\x77\x6F'  # o   w o
+; code += b'\x72\x6C\x64\x21'  # r l d !
+; code += b'\x0A'              # .
--- asm_examples/arm64_macos/print_adrp_autolen.asm
+++ asm_examples/arm64_macos/print_adrp_autolen.asm
@@ -16,3 +16,17 @@ _main:
 .data
 	msg: .ascii "Hello world!\n"
 	.equ msg_len, . -msg
+
+
+; code = b''
+; code += b'\x20\x00\x80\xD2'  # MOV X0, #1
+; code += b'\x01\x00\x00\x90'  # ADRP X1, 0x0 <LTMP0+0x4> ; compiler provides this instruction
+; code += b'\x21\x70\x00\x91'  # ADD X1, X1, #28          ; manually modified shellcode from ADD X1, X1, #0 to ADD X1, X1, #28
+; code += b'\xA2\x01\x80\xD2'  # MOV X2, #13              ; compiler automatically inserted #13 here
+; code += b'\x90\x00\x80\xD2'  # MOV X16, #4
+; code += b'\x01\x00\x00\xD4'  # SVC #0
+; code += b'\xC0\x03\x5F\xD6'  # RET
+; code += b'\x48\x65\x6C\x6C'  # H e l l
+; code += b'\x6F\x20\x77\x6F'  # o   w o
+; code += b'\x72\x6C\x64\x21'  # r l d !
+; code += b'\x0A'              # .
